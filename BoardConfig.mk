055d92eb6dad70a5dcd502d4fb2be0850ab55556
marluk <archmartinoff@gmail.com>
2015-01-20 20:03:34 +0200

    keep furnace defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@
 -include device/oppo/msm8974-common/BoardConfigCommon.mk
 
 # Kernel
-TARGET_KERNEL_CONFIG := slim_bacon_defconfig
+TARGET_KERNEL_CONFIG := furnace_bacon_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_CUSTOM_BOOTIMG_MK := device/oneplus/bacon/mkbootimg.mk
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.8-14.04/bin/arm-linux-gnueabihf-
